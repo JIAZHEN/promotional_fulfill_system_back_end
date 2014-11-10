155fe9646fee8a80e9bd966562bce231e4904742
jiazhen <fdf515@gmail.com>
2014-11-10 23:39:18 +0000

    better DSL for rule

--- lib/rule.rb
+++ lib/rule.rb
@@ -1,43 +1,20 @@
 class Rule
-  KEYS = [:items, :total]
+  KEYS = [:items, :balance]
 
-  def initialize(rule_type, cond = {})
-    @rule_type = rule_type
-    @cond = cond
+  def initialize(options = {}, &block)
     KEYS.each do |key|
+      instance_variable_set("@#{key}", options[key]) if options[key]
       self.class.send(:define_method, key) { instance_variable_get("@#{key}") }
     end
+    @calculator = block
   end
 
-  def eligible_for?(items_or_balance)
-    case @rule_type
-    when :total
-      items_or_balance.send(@cond[:comparison], @cond[:threshold])
-    when :price_drop
-      item = @cond[:item]
-      items_or_balance[item][:qty] >= @cond[:qty]
-    else
-      raise ArgumentError.new('Undefined rule type.')
-    end
+  def match?(cart)
+    (items.nil? || items.all?{|item,qty| cart[item] >= qty}) &&
+    (balance.nil? || balance.all?{|sym, v| cart.balance.send(sym, v)})
   end
 
-  def apply_to(items_or_balance)
-    case @rule_type
-    when :total
-      if @cond[:discount].include?('%')
-        items_or_balance * (1 - @cond[:discount].to_f / 100)
-      else
-        items_or_balance - @cond[:discount].to_f
-      end
-    when :price_drop
-      item = @cond[:item]
-      items_or_balance[item][:price] = @cond[:drop_to]
-    else
-      raise ArgumentError.new('Undefined rule type.')
-    end
-  end
-
-  def is_for_total?
-    @rule_type == :total
+  def total(cart)
+    @calculator.call(cart)
   end
 end
